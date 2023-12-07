7cfe1fb1a5d1c98dfef61a3eb807e3a026bccbed
Muhammad Waqar <muwaqar.qsol@gmail.com>
2023-12-07 14:34:36 +0500

    authentication added through clerk

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -4,9 +4,12 @@ import { Footer } from "./_components/footer";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
+    //layout for root page
     <div className="h-full bg-slate-100">
+      {/* navbar component */}
       <Navbar />
       <main className="pt-20">{children}</main>
+      {/* Footer Component */}
       <Footer />
     </div>
   );
---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Taskify",
+  description:
+    "Collaborative, manage project, and reach new productivity peaks",
+};
